81ae35bf01f8c60b680afcd1f6af42f66b511614
Boluwaji Akinsefunmi <bolu.akinsefunmi@gmail.com>
2021-12-25 19:36:36 +0100

    eligibility checker service created

--- server/controllers/imports.js
+++ server/controllers/imports.js
@@ -1,10 +1,12 @@
 const LotteryUser = require("../models/LotteryUser"),
+    { CheckEligibility } = require("../services/eligibility"),
     { RandomAmount } = require("../services/random.amount"),
     { SendAirtime } = require("../services/send.airtime");
 
 
 module.exports = {
     LotteryUser,
+    CheckEligibility,
     RandomAmount,
     SendAirtime
 }
---
+++ server/services/eligibility.js
@@ -0,0 +1,9 @@
+exports.CheckEligibility = (
+    phoneNumber = 08103412973, 
+    fingerprint = "hiuyrtersxd978654kjgh",
+    LotteryUser
+) => {
+    const check = await LotteryUser.findOne({ $or: [{ phoneNumber, fingerprint}] });
+    if (check) return false;
+    return true;
+}
