eeba8baa1d33f680852ee528472d631affda1ee1
Boluwaji Akinsefunmi <bolu.akinsefunmi@gmail.com>
2021-12-25 21:08:49 +0100

    frontend app bar settled

--- layouts/default.vue
+++ layouts/default.vue
@@ -1,84 +1,45 @@
 <template>
   <v-app dark>
-    <v-navigation-drawer
-      v-model="drawer"
-      :mini-variant="miniVariant"
-      :clipped="clipped"
-      fixed
-      app
-    >
-      <v-list>
-        <v-list-item
-          v-for="(item, i) in items"
-          :key="i"
-          :to="item.to"
-          router
-          exact
-        >
-          <v-list-item-action>
-            <v-icon>{{ item.icon }}</v-icon>
-          </v-list-item-action>
-          <v-list-item-content>
-            <v-list-item-title v-text="item.title" />
-          </v-list-item-content>
-        </v-list-item>
-      </v-list>
-    </v-navigation-drawer>
     <v-app-bar
-      :clipped-left="clipped"
-      fixed
+      absolute
+      dark
+      color="#255561"
+      shrink-on-scroll
+      prominent
+      src="https://raw.githubusercontent.com/bedimcode/responsive-christmas-website/main/assets/img/gift3.png"
+      fade-img-on-scroll
+      scroll-target="#page-body"
+      scroll-threshold="500"
       app
     >
-      <v-app-bar-nav-icon @click.stop="drawer = !drawer" />
-      <v-btn
-        icon
-        @click.stop="miniVariant = !miniVariant"
-      >
-        <v-icon>mdi-{{ `chevron-${miniVariant ? 'right' : 'left'}` }}</v-icon>
-      </v-btn>
-      <v-btn
-        icon
-        @click.stop="clipped = !clipped"
-      >
-        <v-icon>mdi-application</v-icon>
-      </v-btn>
-      <v-btn
-        icon
-        @click.stop="fixed = !fixed"
-      >
-        <v-icon>mdi-minus</v-icon>
-      </v-btn>
+      <template v-slot:img="{ props }">
+        <v-img
+          v-bind="props"
+          gradient="to top right, rgba(55,236,186,.7), rgba(25,32,72,.7)"
+        ></v-img>
+      </template>
       <v-toolbar-title v-text="title" />
       <v-spacer />
-      <v-btn
-        icon
-        @click.stop="rightDrawer = !rightDrawer"
+      <v-switch 
+        v-model="isDark" 
+        :append-icon="
+          `mdi-${ isDark ? 'weather-night' : 'white-balance-sunny' }`
+        "
+        @change="$vuetify.theme.dark = isDark"
       >
-        <v-icon>mdi-menu</v-icon>
-      </v-btn>
+      </v-switch>
     </v-app-bar>
     <v-main>
-      <v-container>
-        <Nuxt />
-      </v-container>
+      <v-sheet
+        id="page-body"
+        class="overflow-y-auto"
+        max-height="600"
+      >
+        <v-container style="height: 1500px;">
+          <Nuxt />
+        </v-container>
+      </v-sheet>
     </v-main>
-    <v-navigation-drawer
-      v-model="rightDrawer"
-      :right="right"
-      temporary
-      fixed
-    >
-      <v-list>
-        <v-list-item @click.native="right = !right">
-          <v-list-item-action>
-            <v-icon light>
-              mdi-repeat
-            </v-icon>
-          </v-list-item-action>
-          <v-list-item-title>Switch drawer (click me)</v-list-item-title>
-        </v-list-item>
-      </v-list>
-    </v-navigation-drawer>
     <v-footer
       :absolute="!fixed"
       app
@@ -93,25 +54,14 @@ export default {
   data () {
     return {
       clipped: false,
+      isDark: true,
       drawer: false,
       fixed: false,
-      items: [
-        {
-          icon: 'mdi-apps',
-          title: 'Welcome',
-          to: '/'
-        },
-        {
-          icon: 'mdi-chart-bubble',
-          title: 'Inspire',
-          to: '/inspire'
-        }
-      ],
       miniVariant: false,
       right: true,
       rightDrawer: false,
-      title: 'Vuetify.js'
+      title: 'Roll Some Dice'
     }
-  }
+  },
 }
 </script>
